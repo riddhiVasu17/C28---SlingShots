ee9ac796af4599d17e19ef400b94336524c195b2
Riddhi Vasu <69344418+riddhiVasu17@users.noreply.github.com>
2020-10-09 14:57:03 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,38 @@
+class slingshot {
+
+    constructor(bodyA, pointB) {
+
+      var option = {
+          bodyA : bodyA,
+          pointB : pointB,
+          stiffness : 0.4,
+          length : 10
+      }
+
+        this.pointB = pointB; 
+
+        this.sling = Constraint.create(option);
+        World.add(world, this.sling);
+
+
+    }
+
+    fly() {
+
+        this.sling.bodyA = null;
+
+    }
+
+    display() {
+
+        if (this.sling.bodyA) {
+
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+}
